e230caf7db70e9c6c0805d577561099659b1c44c
Emin Muhammadi <39090538+eminmuhammadi@users.noreply.github.com>
2025-05-22 22:40:20 +0400

    Items in cart parametrized

--- README.txt
+++ README.txt
@@ -6,7 +6,7 @@ $ pip install pytest pytest-html playwright pytest-playwright pytest_tagging
 
 $ playwright install
 
-$ pytest tests/auth.py -v --tags JIRA-0001 --browser=chromium --slowmo 500 --headed --html=report.html
+$ pytest tests/. -v --tags JIRA-0001 --browser=chromium --slowmo 500 --headed --html=report.html
 
 [-v]                                - Verbose
 [--tags name]                       - Run specific tags
@@ -17,4 +17,4 @@ $ pytest tests/auth.py -v --tags JIRA-0001 --browser=chromium --slowmo 500 --hea
 
 $ playwright show-trace ./traces/XXX/trace-XXX.zip
 
-$ PWDEBUG=1 pytest tests/auth.py -v --tags auth --browser=chromium --slowmo 500 --headed --html=report.html
+$ PWDEBUG=1 pytest tests/. -v --tags JIRA-0001 --browser=chromium --slowmo 500 --headed --html=report.html
--- tests/test_products.py
+++ tests/test_products.py
@@ -9,42 +9,42 @@ from fixtures.saucedemo import *
 from data.constants import *
 
 
-@pytest.mark.tags("JIRA-0001", "ui", "auth")
+@pytest.mark.tags("JIRA-0001", "ui", "products")
 @pytest.mark.parametrize("login_page", ["standard_user"], indirect=True)
 def test_standard_users_should_be_able_to_see_products(products_page):
     expect(products_page.title_locator).to_have_text(CONSTANTS_PRODUCTS_PAGE_TITLE)
     expect(products_page.page).to_have_url(products_page.url)
 
 
-@pytest.mark.tags("JIRA-0002", "ui", "auth")
+@pytest.mark.tags("JIRA-0002", "ui", "products")
 @pytest.mark.parametrize("login_page", ["problem_user"], indirect=True)
 def test_problem_users_should_be_able_to_see_products(products_page):
     expect(products_page.title_locator).to_have_text(CONSTANTS_PRODUCTS_PAGE_TITLE)
-    expect(products_page.page).to_have_url(f"{CONSTANTS_BASE_URL}/inventory.html")
+    expect(products_page.page).to_have_url(products_page.url)
 
 
-@pytest.mark.tags("JIRA-0003", "ui", "auth")
+@pytest.mark.tags("JIRA-0003", "ui", "products")
 @pytest.mark.parametrize("login_page", ["locked_out_user"], indirect=True)
 def test_locked_out_users_should_not_be_able_to_see_products(login_page):
     expect(login_page.error_message_locator).to_have_text(CONSTANTS_AUTH_ERROR_LOCKED_OUT_MESSAGE)
 
 
-@pytest.mark.tags("JIRA-0004", "ui", "auth")
+@pytest.mark.tags("JIRA-0004", "ui", "products")
 @pytest.mark.parametrize("login_page", ["performance_glitch_user"], indirect=True)
 def test_performance_glitch_users_should_be_able_to_see_products(products_page):
     expect(products_page.title_locator).to_have_text(CONSTANTS_PRODUCTS_PAGE_TITLE)
-    expect(products_page.page).to_have_url(f"{CONSTANTS_BASE_URL}/inventory.html")
+    expect(products_page.page).to_have_url(products_page.url)
 
 
-@pytest.mark.tags("JIRA-0005", "ui", "auth")
+@pytest.mark.tags("JIRA-0005", "ui", "products")
 @pytest.mark.parametrize("login_page", ["error_user"], indirect=True)
 def test_error_users_should_be_able_to_see_products(products_page):
     expect(products_page.title_locator).to_have_text(CONSTANTS_PRODUCTS_PAGE_TITLE)
-    expect(products_page.page).to_have_url(f"{CONSTANTS_BASE_URL}/inventory.html")
+    expect(products_page.page).to_have_url(products_page.url)
 
 
-@pytest.mark.tags("JIRA-0006", "ui", "auth")
+@pytest.mark.tags("JIRA-0006", "ui", "products")
 @pytest.mark.parametrize("login_page", ["visual_user"], indirect=True)
 def test_visual_users_should_be_able_to_see_products(products_page):
     expect(products_page.title_locator).to_have_text(CONSTANTS_PRODUCTS_PAGE_TITLE)
-    expect(products_page.page).to_have_url(f"{CONSTANTS_BASE_URL}/inventory.html")
+    expect(products_page.page).to_have_url(products_page.url)
